ff0d7c0fcadf5b298ce8f4137885ac9831f1d694
Charlie Quetstroey <cquetstroey@hotmail.fr>
2017-06-05 23:30:36 +0200

    feat(router):add router to the Qover app

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class LoginPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Login </h1>
+                <p> React, Reduc and React Router </p>
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
